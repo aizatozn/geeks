716ea0b1dcfd67b5a5a345c8dc7c2fcdac4d997a
Aizat <aizatozn@gmail.com>
2024-01-27 16:35:01 +0600

    Initial Commit

---
+++ geeks_pro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geeks_pro
+//
+//  Created by Aizat Ozbekova on 27/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
